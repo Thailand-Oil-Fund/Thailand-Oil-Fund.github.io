aa7a150c73ae49a6f803d6f7643f06a1ca21a615
Kamol Treewatchararat <Kamol.Treewatchararat@telescope.tv>
2014-06-11 13:44:15 -0700

    use backbone

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global ThailandOilFundGithubIo, $*/
+
+
+window.ThailandOilFundGithubIo = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    ThailandOilFundGithubIo.init();
+});
